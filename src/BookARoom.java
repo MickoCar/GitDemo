7ab53e4f861e0f2164127365a0a43246dbfa969c
MickoCar <68964287+MickoCar@users.noreply.github.com>
2020-08-01 16:02:09 -0400

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -2,6 +2,8 @@ public class BookARoom {
 
     public static void main(String[] args) {
         System.out.println("Micko legenda");
+        
+        System.out.println("Ipak je Micko legenda");
     }
 
 
